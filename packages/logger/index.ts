c3c0f4d2a275b06c2f040f17b2028874a98014a4
Henil Malaviya <henilm.dev@gmail.com>
2024-07-27 17:32:31 +0530

    feat: add `@zap/logger` package

---
+++ packages/logger/index.ts
@@ -0,0 +1,103 @@
+import chalk from "chalk";
+import { env } from "@zap/env";
+import { CONST } from "@zap/const";
+
+/**
+ * Log prefix symbols.
+ */
+const PREFIX = Object.freeze({
+  info: "i",
+  warn: "!",
+  error: "x",
+  success: "+",
+});
+
+/**
+ * Constructs a colored prefix for log messages.
+ *
+ * @param prefix - The prefix symbol.
+ * @param color - The chalk color function.
+ * @returns The colored prefix string.
+ */
+function constructPrefix(
+  prefix: string,
+  color: (text: string) => string
+): string {
+  return color(`[${prefix}]`);
+}
+
+/**
+ * Checks if logging is enabled based on the environment variable.
+ *
+ * @returns True if logging is enabled, false otherwise.
+ */
+function canLog(): boolean {
+  return env.getBool(CONST.ENV.ENABLE_LOGGER);
+}
+
+/**
+ * Logs a message to the console with a colored prefix and timestamp.
+ *
+ * @param prefix - The log prefix symbol.
+ * @param color - The chalk color function.
+ * @param data - The data to log.
+ */
+function log(
+  prefix: string,
+  color: (text: string) => string,
+  ...data: any[]
+): void {
+  if (!canLog()) return;
+
+  const prefixString = constructPrefix(prefix, color);
+  const date = chalk.gray(new Date().toISOString());
+
+  console.log(`${prefixString} ${date}`, ...data);
+}
+
+/**
+ * Logs an informational message.
+ *
+ * @param data - The data to log.
+ */
+function info(...data: any[]): void {
+  log(PREFIX.info, chalk.cyanBright, ...data);
+}
+
+/**
+ * Logs a warning message.
+ *
+ * @param data - The data to log.
+ */
+function warn(...data: any[]): void {
+  log(PREFIX.warn, chalk.yellowBright, ...data);
+}
+
+/**
+ * Logs an error message.
+ *
+ * @param data - The data to log.
+ */
+function error(...data: any[]): void {
+  log(PREFIX.error, chalk.redBright, ...data);
+}
+
+/**
+ * Logs a success message.
+ *
+ * @param data - The data to log.
+ */
+function success(...data: any[]): void {
+  log(PREFIX.success, chalk.greenBright, ...data);
+}
+
+/**
+ * Logger object with methods to log different levels of messages.
+ */
+export const logger = {
+  log,
+  info,
+  warn,
+  error,
+  success,
+};
