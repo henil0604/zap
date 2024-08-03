6d7ab6ae60b0049e99ed6b861da9a7788b200e04
Henil Malaviya <henilm.dev@gmail.com>
2024-08-03 19:04:37 +0530

    chore: required constants

--- apps/backend/src/const/index.ts
+++ apps/backend/src/const/index.ts
@@ -1,5 +1,16 @@
 import * as ENV from "./env";
+import * as AUTH from "./auth";
+
+const RESPONSE_CODES = {
+  EXISTS: "EXISTS",
+  CREATED: "CREATED",
+  NOT_FOUND: "NOT_FOUND",
+  INVALID: "INVALID",
+  OK: "OK",
+} as const;
 
 export const CONST = {
   ENV,
-};
+  AUTH,
+  RESPONSE_CODES,
+} as const;
