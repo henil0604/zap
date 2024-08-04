f2b153a005d840114ec96341a012d3eb92efdfd6
Henil Malaviya <henilm.dev@gmail.com>
2024-08-04 18:29:54 +0530

    feat: `/session` route

--- apps/backend/src/routes/auth/index.ts
+++ apps/backend/src/routes/auth/index.ts
@@ -1,19 +1,34 @@
-import { Hono } from "hono";
 import { EmailBasedAuthRoute } from "@/routes/auth/email-based";
 import { AuthService } from "@/services/auth";
 import { createResponse } from "@/utils/createResponse";
 import { CONST } from "@/const";
+import { createHono } from "@/utils/createHono";
+import { PrivateRouteMiddleware } from "@/middlewares/auth";
 
 export const AuthRoute = createHono();
 
 AuthRoute.route("/email", EmailBasedAuthRoute);
 
-AuthRoute.get("/logout", async (c) => {
+AuthRoute.get("/session", PrivateRouteMiddleware(), async (c) => {
+  const user = c.var.user;
+
+  return c.json(
+    createResponse(false, CONST.RESPONSE_CODES.OK, "Session found", user),
+    200
+  );
+});
+
+AuthRoute.post("/logout", PrivateRouteMiddleware(), async (c) => {
+  const sessionId = c.var.session.id;
+
+  await AuthService.invalidateSession(sessionId);
+
   AuthService.clearSessionCookie({
     context: c,
   });
 
   return c.json(
-    createResponse(false, CONST.RESPONSE_CODES.OK, "Logout Successful")
+    createResponse(false, CONST.RESPONSE_CODES.OK, "Logout Successful"),
+    200
   );
 });
